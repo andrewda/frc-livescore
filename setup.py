e83452c9266f6c857b621e4ea2e41e19c2d3bfc2
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-07-08 07:21:05 -0700

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='livescore',
-    version='0.0.2',
+    version='0.0.3',
     description='Get FRC scores from an image',
     author='Andrew Dassonville',
     author_email='dassonville.andrew@gmail.com',
