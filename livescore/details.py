2cdba0bc71e8c4b85d68daec1ed6c87501d909e0
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-07-09 21:32:51 -0700

    Fix new crash

--- livescore/details.py
+++ livescore/details.py
@@ -2,8 +2,9 @@ class Alliance:
     def __init__(self, score=None, teams=[]):
         print 'score'
         print score
-        print score.isdigit()
-        self.score = int(score) if score and score.isdigit() else None
+        if score:
+            print score.isdigit()
+        self.score = int(score) if score is not None and score.isdigit() else None
         self.teams = teams
 
 
@@ -13,6 +14,6 @@ class OngoingMatchDetails:
         print time
         print time.isdigit()
         self.match = match
-        self.time = int(time) if time and time.isdigit() else None
+        self.time = int(time) if time is not None and time.isdigit() else None
         self.red = red
         self.blue = blue
