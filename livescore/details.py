9c4ed660cd1c81bd078d73bcdec1379f2ec2a868
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-07-09 21:29:37 -0700

    Debug failing tests

--- livescore/details.py
+++ livescore/details.py
@@ -1,11 +1,17 @@
 class Alliance:
     def __init__(self, score=None, teams=[]):
+        print 'score'
+        print score
+        print score.isdigit()
         self.score = int(score) if score and score.isdigit() else None
         self.teams = teams
 
 
 class OngoingMatchDetails:
     def __init__(self, match=None, time=None, red=Alliance(), blue=Alliance()):
+        print 'time'
+        print time
+        print time.isdigit()
         self.match = match
         self.time = int(time) if time and time.isdigit() else None
         self.red = red
