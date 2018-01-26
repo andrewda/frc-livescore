e52369cf64063422ff9570a93ee727fac146df58
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-01-26 08:51:13 -0800

    Remove unneeded semicolons

--- build_opencv.sh
+++ build_opencv.sh
@@ -2,21 +2,21 @@ if [ -n "$(ls -A opencv/build)" ];
 then
     # We're using a cached version of our OpenCV build
     echo "Cache found - using that."
-    cd opencv;
-    git init;
-    git remote add origin https://github.com/opencv/opencv.git;
-    git fetch origin --tags;
-    git checkout 3.1.0;
+    cd opencv
+    git init
+    git remote add origin https://github.com/opencv/opencv.git
+    git fetch origin --tags
+    git checkout 3.1.0
 else
     # No OpenCV cache – clone and make the files
     echo "No cache found - cloning and making files."
-    rm -r opencv;
-    git clone https://github.com/opencv/opencv.git;
-    cd opencv;
-    git fetch origin --tags;
-    git checkout 3.1.0;
-    mkdir build;
-    cd build;
+    rm -r opencv
+    git clone https://github.com/opencv/opencv.git
+    cd opencv
+    git fetch origin --tags
+    git checkout 3.1.0
+    mkdir build
+    cd build
     cmake -D CMAKE_BUILD_TYPE=RELEASE \
           -D CMAKE_INSTALL_PREFIX=/usr/local \
           -D WITH_TBB=ON \
@@ -30,5 +30,5 @@ else
           -D BUILD_EXAMPLES=ON \
           -D WITH_QT=ON \
           -D WITH_OPENGL=ON ..
-    make -j8;
+    make -j8
 fi
