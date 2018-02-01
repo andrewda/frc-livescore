bc4872f858d181fcd5c7ada569696b9b269120af
Eugene Fang <fang.eugene@gmail.com>
2018-02-01 15:28:58 -0500

    Handle any scale overlay with SURF

--- livescore/details.py
+++ livescore/details.py
@@ -1,12 +1,12 @@
 class Alliance:
     def __init__(self, score=None, teams=[]):
-        self.score = int(score) if score is not None and score.isdigit() else None
+        self.score = score
         self.teams = teams
 
 
 class OngoingMatchDetails:
     def __init__(self, match=None, time=None, red=Alliance(), blue=Alliance()):
         self.match = match
-        self.time = int(time) if time is not None and time.isdigit() else None
+        self.time = time
         self.red = red
         self.blue = blue
--- livescore/main.py
+++ livescore/main.py
@@ -10,36 +10,9 @@ from details import Alliance, OngoingMatchDetails
 
 
 class Livescore:
-    def __init__(self, options={}):
+    def __init__(self, debug=False):
         local_path = pkg_resources.resource_filename(__name__, 'tessdata')
         template_path = pkg_resources.resource_filename(__name__, 'templates')
-
-        templates = {
-            'scoreboard': template_path + '/scoreboard.png',
-            'scores': template_path + '/scores.png',
-            'time': template_path + '/time.png',
-            'top': template_path + '/top.png'
-        }
-
-        if 'scoreboard' not in options:
-            self.TEMPLATE_SCOREBOARD = cv2.imread(templates['scoreboard'], 0)
-        else:
-            self.TEMPLATE_SCOREBOARD = options['scoreboard']
-
-        if 'scores' not in options:
-            self.TEMPLATE_SCORES = cv2.imread(templates['scores'], 0)
-        else:
-            self.TEMPLATE_SCORES = options['scores']
-
-        if 'time' not in options:
-            self.TEMPLATE_TIME = cv2.imread(templates['time'], 0)
-        else:
-            self.TEMPLATE_TIME = options['time']
-
-        if 'top' not in options:
-            self.TEMPLATE_TOP = cv2.imread(templates['top'], 0)
-        else:
-            self.TEMPLATE_TOP = options['top']
 
         self.WHITE_LOW = np.array([185, 185, 185])
         self.WHITE_HIGH = np.array([255, 255, 255])
@@ -48,110 +21,176 @@ class Livescore:
         self.BLACK_HIGH = np.array([135, 135, 155])
 
         self.local_path = local_path
+        self._debug = debug
 
-    def matchTemplate(self, img, template):
-        res = cv2.matchTemplate(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY),
-                                template,
-                                cv2.TM_CCOEFF_NORMED)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-        top_left = max_loc
-        bottom_right = (top_left[0] + template.shape[1],
-                        top_left[1] + template.shape[0])
+        self._transform = None  # scale, tx, ty
 
-        return top_left, bottom_right
+        # Setup feature detector and matcher
+        self._detector = cv2.xfeatures2d.SURF_create()
 
-    def getScoreboard(self, img):
-        template_width = self.TEMPLATE_SCOREBOARD.shape[1]
-        img_width = img.shape[1]
-        template = imutils.resize(self.TEMPLATE_SCOREBOARD,
-                                  width=int(template_width/1280.0*img_width))
-        top_left, bottom_right = self.matchTemplate(img, template)
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        self._flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-        return img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+        self.MIN_MATCH_COUNT = 10
 
-    def getTopBar(self, img):
-        template_width = self.TEMPLATE_TOP.shape[1]
-        img_width = img.shape[1]
-        template = imutils.resize(self.TEMPLATE_TOP,
-                                  width=int(template_width/1280.0*img_width))
-        top_left, bottom_right = self.matchTemplate(img, template)
+        # Compute score overlay keypoints and descriptors
+        self._TEMPLATE_SCALE = 0.5  # lower is faster
+        template = cv2.imread(template_path + '/score_overlay_2017.png')
+        shape = template.shape
+        template = cv2.resize(template, (
+            np.int32(np.round(shape[1] * self._TEMPLATE_SCALE)),
+            np.int32(np.round(shape[0] * self._TEMPLATE_SCALE))
+        ))
+        self._kp1, self._des1 = self._detector.detectAndCompute(template, None)
 
-        located = img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-        h, w = located.shape[:2]
+    def _findScoreOverlay(self, img):
+        # Finds and sets the 2d transform of the overlay in the image
+        # Sets the transform to None if the overlay is not found
 
-        return located[:, int(w*0.125):int(w*0.5)]
+        kp2, des2 = self._detector.detectAndCompute(img, None)
+        matches = self._flann.knnMatch(self._des1, des2, k=2)
 
-    def getTimeArea(self, img):
-        template_width = self.TEMPLATE_TIME.shape[1]
-        img_width = img.shape[1]
-        template = imutils.resize(self.TEMPLATE_TIME,
-                                  width=int(template_width/1280.0*img_width))
-        top_left, bottom_right = self.matchTemplate(img, template)
+        # Store all the good matches as per Lowe's ratio test
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
 
-        located = img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
-        h, w = located.shape[:2]
+        if len(good) > self.MIN_MATCH_COUNT:
+            src_pts = np.float32([ self._kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
 
-        return located[int(h*0.16):int(h*0.84), int(w*0.42):int(w*0.58)]
+            t = cv2.estimateRigidTransform(src_pts, dst_pts, False)
+            if t is not None:
+                self._transform = {
+                    'scale': t[0, 0],
+                    'tx': t[0, 2],
+                    'ty': t[1, 2],
+                }
+                return
 
-    def getScoreArea(self, img):
-        template_width = self.TEMPLATE_SCORES.shape[1]
-        img_width = img.shape[1]
-        template = imutils.resize(self.TEMPLATE_SCORES,
-                                  width=int(template_width/1280.0*img_width))
-        top_left, bottom_right = self.matchTemplate(img, template)
+        print("Not enough matches are found - {}/{}".format(len(good), self.MIN_MATCH_COUNT))
+        self._transform = None
 
-        return img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+    def _transformPoint(self, point):
+        # Transforms a point from template coordinates to image coordinates
+        scale = self._transform['scale'] * self._TEMPLATE_SCALE
+        tx = self._transform['tx']
+        ty = self._transform['ty']
+        return np.int32(np.round(point[0] * scale + tx)), np.int32(np.round(point[1] * scale + ty))
 
-    def getRedScoreArea(self, img):
-        score_area = self.getScoreArea(img)
-        return score_area[:, int((score_area.shape[1]/2)*0.05):int((score_area.shape[1]/2)*0.95)]
+    def _cornersToBox(self, tl, br):
+        return np.array([
+            [tl[0], tl[1]],
+            [br[0], tl[1]],
+            [br[0], br[1]],
+            [tl[0], br[1]]
+        ])
 
-    def getBlueScoreArea(self, img):
-        score_area = self.getScoreArea(img)
-        return score_area[:, int((score_area.shape[1]/2)*1.05):int(score_area.shape[1]*0.95)]
+    def _getImgCropThresh(self, img, tl, br, white=False):
+        cropped = img[tl[1]:br[1], tl[0]:br[0]]
+        if white:
+            return cv2.inRange(cropped, self.WHITE_LOW, self.WHITE_HIGH)
+        else:
+            return cv2.inRange(cropped, self.BLACK_LOW, self.BLACK_HIGH)
 
-    def read(self, img):
-        scoreboard = self.getScoreboard(img)
+    def _parseDigits(self, img):
+        config = '--psm 8 -l consolas --tessdata-dir {} \
+            -c tessedit_char_whitelist=1234567890 digits'.format(self.local_path)
+        string = pytesseract.image_to_string(
+            Image.fromarray(img),
+            config=config).strip()
+        if string:
+            return int(string)
+        else:
+            return 0
 
-        top_cropped = self.getTopBar(scoreboard)
-        time_cropped = self.getTimeArea(scoreboard)
-        red_cropped = self.getRedScoreArea(scoreboard)
-        blue_cropped = self.getBlueScoreArea(scoreboard)
+    def _drawBox(self, img, box, color):
+        cv2.polylines(img, [box], True, color, 2, cv2.LINE_AA)
 
-        top_cropped = cv2.inRange(top_cropped,
-                                  self.BLACK_LOW,
-                                  self.BLACK_HIGH)
-        time_cropped = cv2.inRange(time_cropped,
-                                   self.BLACK_LOW,
-                                   self.BLACK_HIGH)
-        blue_cropped = cv2.inRange(blue_cropped,
-                                   self.WHITE_LOW,
-                                   self.WHITE_HIGH)
-        red_cropped = cv2.inRange(red_cropped,
-                                  self.WHITE_LOW,
-                                  self.WHITE_HIGH)
+    def _getMatch(self, img, debug_img):
+        tl = self._transformPoint((155, 6))
+        br = self._transformPoint((632, 43))
 
-        long_match = pytesseract.image_to_string(Image.fromarray(top_cropped),
-                                                 config='--psm 7').strip()
+        long_match = pytesseract.image_to_string(
+            Image.fromarray(self._getImgCropThresh(img, tl, br)), config='--psm 7').strip()
+
         match = None
         m = re.search('([a-zA-z]+) ([1-9]+)( of ...?)?', long_match)
         if m is not None:
             match = m.group(1) + ' ' + m.group(2)
 
-        config = '--psm 8 -l consolas --tessdata-dir {} \
-                  -c tessedit_char_whitelist=1234567890 digits'.format(self.local_path)
+        if self._debug:
+            box = self._cornersToBox(tl, br)
+            self._drawBox(debug_img, box, (0, 255, 0))
 
-        time_remaining = pytesseract.image_to_string(
-                                            Image.fromarray(time_cropped),
-                                            config=config).strip()
-        blue_score = pytesseract.image_to_string(
-                                            Image.fromarray(blue_cropped),
-                                            config=config).strip()
-        red_score = pytesseract.image_to_string(
-                                            Image.fromarray(red_cropped),
-                                            config=config).strip()
+        return match
 
-        return OngoingMatchDetails(match=match,
-                                   time=time_remaining,
-                                   red=Alliance(score=red_score),
-                                   blue=Alliance(score=blue_score))
+    def _getScores(self, img, debug_img):
+        # Left score limits
+        left_tl = self._transformPoint((491, 88))
+        left_br = self._transformPoint((639, 160))
+        # Right score limits
+        right_tl = self._transformPoint((639, 88))
+        right_br = self._transformPoint((789, 160))
+        # Sample point to determine red/blue side
+        color_point = self._transformPoint((496, 95))
+
+        left_score = self._parseDigits(self._getImgCropThresh(img, left_tl, left_br, white=True))
+        right_score = self._parseDigits(self._getImgCropThresh(img, right_tl, right_br, white=True))
+
+        color_sample = img[color_point[1], color_point[0], :]
+        is_flipped = color_sample[0] > color_sample[2]  # More blue than red
+
+        if is_flipped:
+            red_score = right_score
+            blue_score = left_score
+        else:
+            red_score = left_score
+            blue_score = right_score
+
+        if self._debug:
+            left_box = self._cornersToBox(left_tl, left_br)
+            right_box = self._cornersToBox(right_tl, right_br)
+            self._drawBox(debug_img, left_box, (255, 255, 0) if is_flipped else (255, 0, 255))
+            self._drawBox(debug_img, right_box, (255, 0, 255) if is_flipped else (255, 255, 0))
+            cv2.circle(debug_img, color_point, 2, (0, 255, 0), -1)
+
+        return red_score, blue_score, is_flipped
+
+    def _getTimeRemaining(self, img, debug_img):
+        tl = self._transformPoint((605, 56))
+        br = self._transformPoint((672, 82))
+
+        time_remaining = self._parseDigits(self._getImgCropThresh(img, tl, br))
+
+        if self._debug:
+            box = self._cornersToBox(tl, br)
+            self._drawBox(debug_img, box, (0, 255, 0))
+
+        return time_remaining
+
+    def read(self, img):
+        img = cv2.resize(img, (1280, 720))
+        self._findScoreOverlay(img)
+
+        if self._transform is not None:
+            debug_img = None
+            if self._debug:
+                debug_img = img.copy()
+
+            match = self._getMatch(img, debug_img)
+            red_score, blue_score, is_flipped = self._getScores(img, debug_img)
+            time_remaining = self._getTimeRemaining(img, debug_img)
+
+            if self._debug:
+                cv2.imshow("ROIs", debug_img)
+                cv2.waitKey(0)
+
+            return OngoingMatchDetails(
+                match=match,
+                time=time_remaining,
+                red=Alliance(score=red_score),
+                blue=Alliance(score=blue_score))
